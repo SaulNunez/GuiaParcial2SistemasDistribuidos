3dfa145c3883f59614c7a64a2ad4a12ff448f327
Saul Nunez <saul.nunez99@gmail.com>
2019-03-12 21:27:07 -0600

    Algoritmo para adivinar numeros comenzado. Algoritmo para definir un numero.

--- src/guiaparcial2sistemasdistribuidos/Adivinador.java
+++ src/guiaparcial2sistemasdistribuidos/Adivinador.java
@@ -5,12 +5,16 @@
  */
 package guiaparcial2sistemasdistribuidos;
 
+import java.util.Random;
+
 /**
  *
  * @author sauln
  */
-public class Adivinador extends Thread implements IAdivinador {
+public class Adivinador extends Thread {
     private final Generador generador;
+    private int min = 1;
+    private int max = 10;
     
     public Adivinador(Generador generador){
         this.generador = generador;
@@ -18,10 +22,16 @@ public class Adivinador extends Thread implements IAdivinador {
     
     @Override
     public void run(){
-    
+        Random random = new Random();
+        // Next int saca numeros desde el cero hasta <max
+        // Por eso usamos el floor para obtener numeros desde el cero hasta max y en int
+        // Ademas, nuestro rango no es cero, así que sumamos min a cero
+        Generador.Pistas pistaActual = generador.intento((int)Math.floor(random.nextInt(max - min + 1)) + min);
+        while(pistaActual != Generador.Pistas.Correcto){
+        
+        }
     }
 
-    @Override
     public void IntentosTerminados() {
         System.out.println("Adivinador: :(");
     }
